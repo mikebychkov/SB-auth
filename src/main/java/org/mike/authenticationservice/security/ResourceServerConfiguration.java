74a4bc75644c6cdf28818601ca230c2b8347eeb7
mikebychkov <mikebychkov@github.com>
2021-02-22 02:23:20 +0500

    authentication svc up'n running

---
+++ src/main/java/org/mike/authenticationservice/security/ResourceServerConfiguration.java
@@ -0,0 +1,18 @@
+package org.mike.authenticationservice.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+
+@Configuration // OAUTH2 SERVER SECURITY
+public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity http) throws Exception{
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/actuator/**").permitAll()
+                //.antMatchers("/user/**").permitAll()
+                .antMatchers("/**").authenticated();
+    }
+}
